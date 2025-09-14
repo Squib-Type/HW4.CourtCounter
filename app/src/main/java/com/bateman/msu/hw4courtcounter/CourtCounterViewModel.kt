366a98b4bb66a8858b910f97cb2895b2020e8816
Squib-Type <kjohnson133922@gmail.com>
2025-09-14 08:20:15 -0500

    Exercise 5 Done

--- app/src/main/java/com/bateman/msu/hw4courtcounter/CourtCounterViewModel.kt
+++ app/src/main/java/com/bateman/msu/hw4courtcounter/CourtCounterViewModel.kt
@@ -1,14 +1,17 @@
 package com.bateman.msu.hw4courtcounter
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlin.collections.plusAssign
 import kotlin.inc
 
 private const val TAG = "CourtCounterViewModel"
 
+const val CURRENT_SCORE_A = "CURRENT_SCORE_A"
+const val CURRENT_SCORE_B = "CURRENT_SCORE_B"
 
-class CourtCounterViewModel: ViewModel() {
+class CourtCounterViewModel(private val savedStateHandle: SavedStateHandle): ViewModel() {
 
     init {
 
@@ -16,8 +19,12 @@ class CourtCounterViewModel: ViewModel() {
 
     }
 
-    var scoreTeamA = 0
-    var scoreTeamB = 0
+    var scoreTeamA
+        get() = savedStateHandle.get(CURRENT_SCORE_A)?:0
+        set(value) = savedStateHandle.set(CURRENT_SCORE_A, value)
+    var scoreTeamB
+        get() = savedStateHandle.get(CURRENT_SCORE_B)?:0
+        set(value) = savedStateHandle.set(CURRENT_SCORE_B, value)
 
     fun plusOneTeamA(){
         scoreTeamA++
